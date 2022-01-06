535775b60900022fa35e5886b21a9e41441ff794
shomiyu <m1011522@gmail.com>
2022-01-06 20:30:44 +0900

    footer作成

--- index.php
+++ index.php
@@ -307,6 +307,19 @@
     </section>
   </main>
 
+  <footer class="footer">
+    <div class="footer-upper">
+      <div class="container">
+        <p class="footer-logo">
+          <a href="/">
+            <img src="http://placehold.jp/160x48.png" alt="ロゴ">
+          </a>
+        </p>
+        <p>〒000-0000<br>愛知県名古屋市中村区名駅〇丁目〇-〇<br>サンプルビル〇階</p>
+      </div>
+    </div>
+    <p class="copyright"><small>©️ 2022 kitani All Rights Reserved.</small></p>
+  </footer>
   <?php wp_footer(); ?>
 </body>
 </html>
