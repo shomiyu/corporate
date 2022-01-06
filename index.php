d104267ad8f17fa9e8a3b5565db9c334ada44eab
shomiyu <m1011522@gmail.com>
2022-01-06 20:40:02 +0900

    footerテンプレート化

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+
   <main class="main">
     <div class="main-visual">
       <picture>
@@ -275,19 +276,4 @@
     </section>
   </main>
 
-  <footer class="footer">
-    <div class="footer-upper">
-      <div class="container">
-        <p class="footer-logo">
-          <a href="/">
-            <img src="http://placehold.jp/160x48.png" alt="ロゴ">
-          </a>
-        </p>
-        <p>〒000-0000<br>愛知県名古屋市中村区名駅〇丁目〇-〇<br>サンプルビル〇階</p>
-      </div>
-    </div>
-    <p class="copyright"><small>©️ 2022 kitani All Rights Reserved.</small></p>
-  </footer>
-  <?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
