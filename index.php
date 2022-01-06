5867212b5a673ad5ee7c1222c08040068001ca6b
shomiyu <m1011522@gmail.com>
2022-01-06 20:37:37 +0900

    headerのテンプレート化

--- index.php
+++ index.php
@@ -1,36 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
-</head>
-<body>
-  <header class="header">
-    <div class="header-container">
-      <h1 class="header-logo">
-        <a href="/">
-          <img src="http://placehold.jp/160x48.png" alt="ロゴ">
-        </a>
-      </h1>
-
-      <nav class="global-menu">
-        <ul class="menu">
-          <li class="menu-item">
-            <a href="#!">私たちについて</a>
-          </li>
-          <li class="menu-item">
-            <a href="#!">お知らせ</a>
-          </li>
-          <li class="menu-item">
-            <a href="#!">お問い合せ</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-
+<?php get_header(); ?>
   <main class="main">
     <div class="main-visual">
       <picture>
